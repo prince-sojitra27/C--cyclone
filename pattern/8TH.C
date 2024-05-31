2c174f624004834ce3f641e3753c78e122d410d3
Prince Sojitra <sojitraprince880@gmail.com>
2024-05-31 17:15:42 +0530

    Add files via upload

---
+++ pattern/8TH.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{       int i,j,n=1;
+
+	clrscr();
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+		      printf("%d\t",n);
+		      n++;
+		}     printf("\n");
+	}
+
+	getch();
+
+
+}
