2589433de018b0ea8cc7f9b220a3648334aca2e8
Prince Sojitra <sojitraprince880@gmail.com>
2024-06-20 20:02:48 +0530

    Add files via upload

---
+++ 9-1/togle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+main()
+
+{
+    char str[100];
+    int i;
+
+    printf("Enter the name :-");
+    scanf("%[^\n]", &str);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] + 32;
+        }
+    }
+    printf("The uppercase string is :- %s\n", str);
+}
---
+++ 9-1/uppercase.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main()
+
+{
+    char str[100];
+    int i;
+
+    printf("Enter the name :-");
+    scanf("%[^\n]", &str);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+    }
+    printf("The uppercase string is :- %s\n", str);
+}
